79db73b0b0d1f52118bb693b37d8db1584f5a836
stflood <stflood@users.noreply.github.com>
2026-09-21 17:01:00 +0300

    configure supabase publishable key

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,2 +1,2 @@
-window.SUPABASE_URL = "";
-window.SUPABASE_ANON_KEY = "";
+window.SUPABASE_URL = "https://ervyfsfcwfpswsuhohyd.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_-FPPeYJAefBJpDsax711eQ_6GUIoJ3Q";
